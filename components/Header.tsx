c09e688b5920ffc8c0042bd60acc66a08d3f75ba
Kylie <kylieavie@gmail.com>
2022-11-14 19:23:38 +0800

    homepage UI done, need to fix banner text size

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+import { MagnifyingGlassIcon, BellIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+function Header() {
+  // react hook
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // on mount,
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // js
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  // empty dependency
+
+  return (
+    // template string, js, css
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        {/* netflix logo */}
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        {/* react and hero icons */}
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+
+        <BellIcon className="h-6 w-6" />
+
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ components/Row.tsx
@@ -0,0 +1,67 @@
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { useRef, useState } from "react";
+import { Movie } from "../typings";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  title: string;
+  movies: Movie[];
+}
+
+function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+
+    // rowRef has different values
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      // screen size -> clientWidth
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
+  console.log(rowRef.current?.scrollLeft, rowRef.current?.clientWidth);
+
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className={`arrowIcon left-2 ${!isMoved && "hidden"}`}
+          onClick={() => handleClick("left")}
+        />
+
+        {/* movies */}
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+        >
+          {/* thumbnail */}
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
+        <ChevronRightIcon
+          className="arrowIcon right-2"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Row;
